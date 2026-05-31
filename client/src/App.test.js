e1223bb2eb8ec241b8f0889e6a8cd3b6e4c1e79a
soju thomas <sojucr7@gmail.com>
2026-05-31 14:41:18 +0530

    k8s deployment project files

---
+++ client/src/App.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import Test from './Test';
+
+test('renders Im some other page!', () => {
+  render(<Test />);
+  const linkElement = screen.getByText(/Im some other page!/i);
+  
+  expect(linkElement).toBeInTheDocument();
+});;
